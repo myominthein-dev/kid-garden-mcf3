1c6c61d7897e84e228ff63fcfb1d5a37da47d54c
Vee-Scored <myomin9916@gmail.com>
2024-12-26 16:18:15 +0630

    feat:desktop-size finished

--- src/features/public/components/Footer.jsx
+++ src/features/public/components/Footer.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import Container from "./Container";
+import NewsLetter from "./NewsLetter";
+import footerLogo from "../../../assets/homePage/footer_tinyverse.png"
+import { useNavigate } from "react-router-dom";
+import facebook from "../../../assets/homePage/facebook_vec.png"
+import twitter from "../../../assets/homePage/twitter_vec.png"
+import instagram from "../../../assets/homePage/ig_vec.png"
+const Footer = ({bgImg}) => {
+  const nav = useNavigate();
 
-const Footer = () => {
+  const link = (path) => {
+    nav(path);
+  }
   return (
-    <footer className="border-t-2 border-gray-200 antialiased dark:bg-gray-800 mt-auto">
-      I am footer
+    <footer style={{backgroundImage : `url(${bgImg})`}}  className={`border-t-2 h-[832px] bg-[url()] bg-cover bg-no-repeat  border-gray-200 antialiased dark:bg-gray-800 mt-auto`}>
+      <Container>
+        <NewsLetter/>
+        
+        <div className="max-w-[561px] mx-auto relative top-44  h-[316px] ">
+              <div className="flex items-center justify-center">
+                <img  src={footerLogo} alt="" />
+              </div>
+              <p className="font-roboto text-lg lg:px-20 my-5 text-center font-thin">"Find the perfect class tailored to your child's needs and interests."</p>
+              <ul className="text-orange-600 flex items-center justify-evenly leading-4 font-semibold">
+                <li onClick={()=>link('/')} className="cursor-pointer">Home</li>
+                <li onClick={()=>link('about-us')} className="cursor-pointer">About Us</li>
+                <li onClick={()=>link('courses')} className="cursor-pointer">Courses</li>
+                <li onClick={()=>link('contact-us')} className="cursor-pointer">Contact Us</li>
+                <li onClick={()=>link('events')} className="cursor-pointer">Events</li>
+              </ul>
+
+              <div className="max-w-[144px] flex justify-evenly mx-auto my-5">
+                <img src={twitter} alt="" /><img src={facebook} alt="" /><img src={instagram} alt="" />
+              </div>
+
+              <p className="font-roboto leading-4 text-center">© 2024 Tinyverse. All Rights Reserved.</p>
+        </div>
+      </Container>
     </footer>
   );
 };
