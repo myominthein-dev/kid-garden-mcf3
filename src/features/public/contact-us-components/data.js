cb6344ea0847b4aaf905ea1edbbdef55de63674e
Hanhtunaung97 <littlehanhtunaung97>
2025-01-02 23:16:15 +0800

    feat: hero-section finish

---
+++ src/features/public/contact-us-components/data.js
@@ -0,0 +1,27 @@
+import image1 from "../../../assets/contactUsPage/image1.png";
+import image2 from "../../../assets/contactUsPage/image2.png";
+import image3 from "../../../assets/contactUsPage/image3.png";
+
+export const heroContents = [
+  {
+    id: 1,
+    title: "Our Mission",
+    description:
+      "Our mission is to nurture young minds in a safe, joyful, and stimulating environment where every child can explore, learn, and grow. We are dedicated to fostering creativity.",
+    image: image1,
+  },
+  {
+    id: 2,
+    title: "Who We Are",
+    description:
+      "We are a dedicated team of educators and caregivers passionate about creating a nurturing environment for young children. At Tinyverse, we provide a safe, engaging.",
+    image: image2,
+  },
+  {
+    id: 3,
+    title: "Our Approach",
+    description:
+      "Our approach is centered on play-based learning, fostering curiosity and creativity in every child. By combining a nurturing environment with structured activities.",
+    image: image3,
+  },
+];
