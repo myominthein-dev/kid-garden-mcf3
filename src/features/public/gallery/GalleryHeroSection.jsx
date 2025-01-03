ff8ac93c0a1fe8cfee6c3b0644e0fdf40e7f0707
blankverse123000 <blankverse123000@gmail.com>
2025-01-03 20:19:45 +0800

    feat/added TopTitleSection

--- src/features/public/gallery/GalleryHeroSection.jsx
+++ src/features/public/gallery/GalleryHeroSection.jsx
@@ -1,17 +1,14 @@
-import React from "react";
 import Container from "../components/Container";
 
-import ImgGridRow2 from "./components/ImgGridRow2";
-import ImgGridRow3 from "./components/ImgGridRow3";
-import ImgGridRow1 from "./components/ImgGridRow1";
 import SeeMoreBtn from "./components/SeeMoreBtn";
+import TopTitle from "./components/TopTitle";
 import PhotoSection from "./PhotoSection";
 
 const GalleryHeroSection = () => {
   return (
     <Container>
-      <div>GalleryHeroSection</div>
       <div className=" flex flex-col  gap-28">
+        <TopTitle />
         <PhotoSection />
         <SeeMoreBtn />
       </div>
--- src/features/public/gallery/components/ImageDiv.jsx
+++ src/features/public/gallery/components/ImageDiv.jsx
@@ -2,7 +2,7 @@ const ImageDiv = ({ className, src }) => {
   return (
     <>
       <div
-        className={`border-2 border-dashed rounded-md overflow-hidden h-full ${className}`}
+        className={`border-2 border-dashed rounded-2xl border-opacity-50 overflow-hidden h-full ${className} `}
       >
         <img className=" h-full w-full object-cover" src={src} alt="" />
       </div>
