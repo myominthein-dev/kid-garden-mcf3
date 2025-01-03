94e87a57895711b0f6b19eee891ddc0716ddc8a5
Hanhtunaung97 <littlehanhtunaung97>
2025-01-04 01:52:30 +0800

    feat:contact-footer add

--- src/features/public/pages/ContactUsPage.jsx
+++ src/features/public/pages/ContactUsPage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ContactUsHeroSection from "../contact-us-components/ContactUsHeroSection";
 import ContactMarketing from "../contact-us-components/ContactMarketing";
 import ContactTeam from "../contact-us-components/ContactTeam";
-
+import Footer from "../components/Footer";
+import footerBg from "../../../assets/homePage/bg_footer.jpg";
 const ContactUsPage = () => {
   return (
     <>
       <ContactUsHeroSection />
       <ContactMarketing />
       <ContactTeam />
+      <Footer bgImg={footerBg} newsLetterBgColor={"orange"} />
     </>
   );
 };
