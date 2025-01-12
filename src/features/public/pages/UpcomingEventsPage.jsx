3f3727fdc44d146ebcdde747d4744db9c044eaf2
blankverse123000 <blankverse123000@gmail.com>
2025-01-12 12:53:06 +0800

    fix/event responsive without breadcrumb

--- src/features/public/pages/UpcomingEventsPage.jsx
+++ src/features/public/pages/UpcomingEventsPage.jsx
@@ -1,61 +1,102 @@
-import React from 'react'
-import Container from '../components/Container'
-import footerBg from '../../../assets/homePage/bg_footer.jpg'
-import writingInFense from "../../../assets/homePage/writing_in_fense.png"
-import groupingInClas from "../../../assets/homePage/grouping_in_class.png"
-import playingInCompound from '../../../assets/homePage/playing_in_compound.png'
-import CalendarCard from '../home-components/CalendarCard'
-import ropeRace from "../../../assets/homePage/rope_race.png"
-import groupRanning from "../../../assets/homePage/kid_group_ranning.png"
-import makingCircle from "../../../assets/homePage/kids_making_circle.png"
-import playingFootball from "../../../assets/homePage/kid_playing_football.png"
-import spider from "../../../assets/homePage/spider_with_kids.png"
-import hollowenKid from "../../../assets/homePage/kid_with_hollowen.png"
-import Footer from '../components/Footer'
-import greenSq from '../../../assets/events/small_green.png'
-import orangeSq from '../../../assets/events/bigger_orange.png'
-import roseSq from '../../../assets/events/big_rose.png'
-import cloud from "../../../assets/events/orange_cloud.png"
+import Container from "../components/Container";
+import footerBg from "../../../assets/homePage/bg_footer.jpg";
+import writingInFense from "../../../assets/homePage/writing_in_fense.png";
+import groupingInClas from "../../../assets/homePage/grouping_in_class.png";
+import playingInCompound from "../../../assets/homePage/playing_in_compound.png";
+import bgImage from "../../../assets/homePage/homeHeroBg.png";
+import CalendarCard from "../home-components/CalendarCard";
+import ropeRace from "../../../assets/homePage/rope_race.png";
+import groupRanning from "../../../assets/homePage/kid_group_ranning.png";
+import makingCircle from "../../../assets/homePage/kids_making_circle.png";
+import playingFootball from "../../../assets/homePage/kid_playing_football.png";
+import spider from "../../../assets/homePage/spider_with_kids.png";
+import hollowenKid from "../../../assets/homePage/kid_with_hollowen.png";
+import Footer from "../components/Footer";
+import greenSq from "../../../assets/events/small_green.png";
+import orangeSq from "../../../assets/events/bigger_orange.png";
+import roseSq from "../../../assets/events/big_rose.png";
+import cloud from "../../../assets/events/orange_cloud.png";
+import DetailBtn from "../home-components/DetailBtn";
+import BreadCrumb from "../components/BreadCrumb";
 
 const events = [
-    writingInFense,groupingInClas,ropeRace,playingInCompound,groupRanning,makingCircle,playingFootball,spider,hollowenKid
-]
+  writingInFense,
+  groupingInClas,
+  ropeRace,
+  playingInCompound,
+  groupRanning,
+  makingCircle,
+  playingFootball,
+  spider,
+  hollowenKid,
+];
 const UpcomingEventsPage = () => {
   return (
-   <>
-         <Container className={'relative'}>
-        <img className='absolute right-5' src={cloud} alt="" />
-        <div className="relative mx-auto flex justify-center items-center  gap-4 flex-col w-[618px]">
-          <div className='w-20 h-20  absolute -top-5 -left-5'>
-            <img className='absolute right-0 bottom-0' src={greenSq} alt="" />
-            <img className='absolute left-0 top-0 bottom-0 my-auto' src={orangeSq} alt="" />
-            <img className='absolute top-0 right-0' src={roseSq} alt="" />
+    <>
+      <BreadCrumb
+        bgImg={bgImage}
+        leftImg={"hdalh"}
+        lImgWidth={300}
+        rightImg={"dldahl"}
+        title={"Our Courses"}
+        currentPageTitle={"Event"}
+      />
+      <Container className={"relative"}>
+        <img
+          className="absolute xl:inline-block md:hidden xl:right-5  -top-4 right-16"
+          src={cloud}
+          alt=""
+        />
+        <div className="relative md:mx-auto flex md:justify-center md:items-center  gap-4 flex-col md:w-[618px]">
+          <div className="w-20 h-20 xl:inline-block hidden absolute -top-5 -left-5">
+            <img className="absolute right-0 bottom-0" src={greenSq} alt="" />
+            <img
+              className="absolute left-0 top-0 bottom-0 my-auto"
+              src={orangeSq}
+              alt=""
+            />
+            <img className="absolute top-0 right-0" src={roseSq} alt="" />
           </div>
-          <h3 className=" text-orange-500 font-normal text-xl font-pacifico">
+          <h3 className=" text-orange-500 leading-9 font-normal text-xl  font-pacifico">
             Upcoming Events
           </h3>
-          <h1 className=" text-center text-4xl font-poppin font-semibold text-neutral-900">
-          Calendar Of School Event
+          <h1 className=" xl:text-center leading-9 xl:text-4xl text-2xl font-poppin font-semibold text-neutral-900">
+            Calendar Of School Event
           </h1>
-          <p className='text-lg font-roboto text-center text-neutral-700'>
-          We have 9 amazing events planned for this year, filled with fun, learning, and community activities for students and families. Stay tuned and join us to create wonderful memories together!"
+          <p className="md:text-lg text-base font-roboto md:text-center  text-neutral-700">
+            We have 9 amazing events planned for this year, filled with fun,
+            learning, and community activities for students and families. Stay
+            tuned and join us to create wonderful memories together!"
           </p>
         </div>
 
-        <div className='my-10 grid grid-cols-3 gap-4'>
-        {
-            events.map((e,i) => <CalendarCard key={i} imgUrl={e} />)
-        }
+        {/* Card loop  */}
+        <div className="my-10 xl:grid xl:grid-cols-3 justify-between items-center hidden gap-4">
+          {events.map((e, i) => (
+            <CalendarCard key={i} imgUrl={e} />
+          ))}
         </div>
+        <div className="my-10 md:grid xl:hidden hidden justify-between items-center md:grid-cols-2 gap-4">
+          {events.slice(0, 6).map((e, i) => (
+            <CalendarCard key={i} imgUrl={e} />
+          ))}
+        </div>
+        <div className="my-10 md:hidden  grid justify-center  justify-items-center  items-center grid-cols-1 gap-4">
+          {events.slice(0, 3).map((e, i) => (
+            <CalendarCard key={i} imgUrl={e} />
+          ))}
+        </div>
+        <DetailBtn
+          className={" inline-block md:hidden"}
+          content={"View all events"}
+        />
+        <h3 className="  hidden md:grid text-orange-500 text-center py-5 my-5 font-normal text-xl  font-pacifico">
+          Stay Tune For Next Academic Year
+        </h3>
+      </Container>
+      <Footer bgImg={footerBg} color={"orange"} />
+    </>
+  );
+};
 
-        <h3 className=" text-orange-500 text-center py-5 my-5 font-normal text-xl font-pacifico">
-        Stay Tune For Next Academic Year
-          </h3>
-
-    </Container>
-    <Footer bgImg={footerBg} color={"orange"} />
-   </>
-  )
-}
-
-export default UpcomingEventsPage
+export default UpcomingEventsPage;
