10bd95bb0d6715cae3b24ca616aa890382a9b599
blankverse123000 <blankverse123000@gmail.com>
2025-01-15 00:47:24 +0800

    fix/ fixed Home EventCalendar Responsive

--- src/features/public/home-components/EventCalendar.jsx
+++ src/features/public/home-components/EventCalendar.jsx
@@ -1,45 +1,58 @@
-import React from 'react'
-import Container from '../components/Container'
-import DetailBtn from './DetailBtn'
-import CalendarCard from './CalendarCard'
-import playingInCompound from '../../../assets/homePage/playing_in_compound.png'
-import writingInFense from '../../../assets/homePage/writing_in_fense.png'
-import groupingInClass from '../../../assets/homePage/grouping_in_class.png'
+import React from "react";
+import Container from "../components/Container";
+import DetailBtn from "./DetailBtn";
+import CalendarCard from "./CalendarCard";
+import playingInCompound from "../../../assets/homePage/playing_in_compound.png";
+import writingInFense from "../../../assets/homePage/writing_in_fense.png";
+import groupingInClass from "../../../assets/homePage/grouping_in_class.png";
+import groupRanning from "../../../assets/homePage/kid_group_ranning.png";
 
 const images = [
-    {
-        image : playingInCompound, 
-    },
-    {
-        image : writingInFense
-    },
-    {
-        image : groupingInClass
-    }
-]
+  {
+    image: playingInCompound,
+  },
+  {
+    image: writingInFense,
+  },
+  {
+    image: groupRanning,
+  },
+  {
+    image: groupingInClass,
+  },
+];
 
 const EventCalendar = () => {
   return (
     <Container>
-        <div className='flex justify-between  items-end'>
-            <div className='max-w-[503px]'>
-                <h5 className="font-pacifico text-xl text-orange-500 mb-4">
-                   Upcoming Events
-                </h5>
-                <h2 className='text-4xl font-semibold leading-[54px]'>Calendar Of Upcoming School Event</h2>
-            </div>
-            <div >
-                <DetailBtn content={'View all events'} />
-            </div>
+      <div className="flex justify-between  md:items-end ">
+        <div className="xl:max-w-[503px] flex flex-col gap-4">
+          <h5 className="font-pacifico text-xl text-orange-500 mb-4">
+            Upcoming Events
+          </h5>
+          <h2 className="xl:text-4xl text-2xl font-semibold ">
+            Calendar Of Upcoming School Event
+          </h2>
         </div>
-
-        <div className=' flex py-5 my-10 justify-between'>
-            {
-                images.map( (img,index) => <CalendarCard key={index} imgUrl={img.image} />)
-            }
+        <div className=" md:inline-block hidden">
+          <DetailBtn content={"View all events"} />
         </div>
+      </div>
+      <div className=" xl:grid xl:grid-cols-3 xl:gap-6 md:hidden grid-cols-1 justify-between  py-5 xl:px-2   grid  gap-4 my-10  ">
+        {images.slice(0, 3).map((img, index) => (
+          <CalendarCard key={index} imgUrl={img.image} />
+        ))}
+      </div>
+      <div className=" md:grid xl:hidden hidden md:grid-cols-2  justify-between  py-5 xl:px-2 px-4 gap-4 my-10  ">
+        {images.map((img, index) => (
+          <CalendarCard key={index} imgUrl={img.image} />
+        ))}
+      </div>
+      <div className=" inline-block md:hidden mt-4 mb-24">
+        <DetailBtn content={"View all events"} />
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default EventCalendar
+export default EventCalendar;
