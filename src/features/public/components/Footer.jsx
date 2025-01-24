8385e6fc6267eb793a1bfd6148d7a6aa7b883f6a
ArrKuu77 <aungkungsan@gmail.com>
2025-01-24 04:07:35 -1100

    bth-error

--- src/features/public/components/Footer.jsx
+++ src/features/public/components/Footer.jsx
@@ -3,19 +3,19 @@ import Container from "./Container";
 import NewsLetter from "./NewsLetter";
 import footerLogo from "../../../assets/homePage/footer_tinyverse.png";
 import { useNavigate } from "react-router-dom";
-import {motion, useInView, useAnimation} from 'framer-motion'
+import { motion, useInView, useAnimation } from "framer-motion";
 import { LuFacebook, LuInstagram, LuTwitter } from "react-icons/lu";
 import { itemVariants, titleVariants } from "../../../utils";
 
 const Footer = ({ bgImg, color }) => {
   const nav = useNavigate();
-  const ref = useRef(null)
-    const isInView = useInView(ref, { once: true, amount: 0.3 })
-    const controls = useAnimation()
-  
-    if (isInView) {
-      controls.start('visible')
-    }
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.3 });
+  const controls = useAnimation();
+
+  if (isInView) {
+    controls.start("visible");
+  }
   const link = (path) => {
     nav(path);
   };
@@ -31,26 +31,64 @@ const Footer = ({ bgImg, color }) => {
           <div className="flex items-center justify-center">
             <img src={footerLogo} />
           </div>
-          <motion.p ref={ref} initial='hidden'animate={controls} variants={titleVariants} className="font-roboto  md:px-40 px-24 text-xs my-5 text-center text-neutral-700">
+          <motion.p
+            ref={ref}
+            initial="hidden"
+            animate={controls}
+            variants={titleVariants}
+            className="font-roboto  md:px-40 px-24 text-xs my-5 text-center text-neutral-700"
+          >
             "Find the perfect class tailored to your child's needs and
             interests."
           </motion.p>
           <ul
             className={`text-${color}-600 flex xl:gap-8 gap-4 xl:text-base  text-xs items-center  justify-center leading-4 font-semibold`}
           >
-            <motion.li ref={ref} initial='hidden' animate={controls} variants={titleVariants} onClick={() => link("/")} className="cursor-pointer">
+            <motion.li
+              ref={ref}
+              initial="hidden"
+              animate={controls}
+              variants={titleVariants}
+              onClick={() => link("/")}
+              className="cursor-pointer"
+            >
               Home
             </motion.li>
-            <motion.li ref={ref} initial='hidden' animate={controls} variants={titleVariants} onClick={() => link("/about-us")} className="cursor-pointer">
+            <motion.li
+              ref={ref}
+              initial="hidden"
+              animate={controls}
+              variants={titleVariants}
+              onClick={() => link("/about-us")}
+              className="cursor-pointer"
+            >
               About Us
             </motion.li>
-            <motion.li ref={ref} initial='hidden' animate={controls} variants={titleVariants} onClick={() => link("/our-courses")} className="cursor-pointer">
+            <motion.li
+              ref={ref}
+              initial="hidden"
+              animate={controls}
+              variants={titleVariants}
+              onClick={() => link("/our-courses")}
+              className="cursor-pointer"
+            >
               Courses
             </motion.li>
-            <motion.li ref={ref} initial='hidden' animate={controls} variants={titleVariants} onClick={() => link("/contact-us")} className="cursor-pointer">
+            <motion.li
+              ref={ref}
+              initial="hidden"
+              animate={controls}
+              variants={titleVariants}
+              onClick={() => link("/contact-us")}
+              className="cursor-pointer"
+            >
               Contact Us
             </motion.li>
-            <motion.li ref={ref} initial='hidden'animate={controls} variants={titleVariants}
+            <motion.li
+              ref={ref}
+              initial="hidden"
+              animate={controls}
+              variants={titleVariants}
               onClick={() => link("/upcoming-events")}
               className="cursor-pointer"
             >
@@ -66,7 +104,13 @@ const Footer = ({ bgImg, color }) => {
             <LuInstagram size={25} />
           </div>
 
-          <motion.p ref={ref} initial='hidden'animate={controls} variants={titleVariants} className="font-roboto leading-4 text-center">
+          <motion.p
+            ref={ref}
+            initial="hidden"
+            animate={controls}
+            variants={titleVariants}
+            className="font-roboto leading-4 text-center"
+          >
             © 2024 Tinyverse. All Rights Reserved.
           </motion.p>
         </div>
