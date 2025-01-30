88723ee4828e787b36d509ac52dcd3e198570323
ArrKuu77 <aungkungsan@gmail.com>
2025-01-30 04:56:58 -1100

    fix/handleMenu-outLine

--- src/features/public/components/HamburgerMenuRoute.jsx
+++ src/features/public/components/HamburgerMenuRoute.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { useInView, useAnimation, motion } from "framer-motion";
 import { itemVariants, titleVariants } from "../../../utils";
-const HamburgerMenuRoute = ({ route, name,toggleMenu }) => {
+const HamburgerMenuRoute = ({ route, name, toggleMenu }) => {
   const [hoverEff, setHoverEff] = useState(false);
   const handalHoverEnter = () => {
     setHoverEff(true);
@@ -11,28 +11,38 @@ const HamburgerMenuRoute = ({ route, name,toggleMenu }) => {
     setHoverEff(false);
   };
 
-  const ref = useRef(null)
-      const isInView = useInView(ref, { once: true, amount: 0.3 })
-      const controls = useAnimation()
-    
-      if (isInView) {
-        controls.start('visible')
-      }
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.3 });
+  const controls = useAnimation();
+  const [mobileOutLine, setMobileOutLine] = useState(false);
+  if (isInView) {
+    controls.start("visible");
+  }
 
   return (
-    <motion.li onClick={toggleMenu} initial='hidden' ref={ref} variants={itemVariants} animate={controls} className="z-50"
+    <motion.li
+      onClick={toggleMenu}
+      initial="hidden"
+      ref={ref}
+      variants={itemVariants}
+      animate={controls}
+      className="z-50"
       onMouseEnter={() => handalHoverEnter()}
       onMouseLeave={() => handalHoverLeave()}
     >
       <NavLink
         to={route}
-        className="block py-2 px-4 text-gray-800 font-pacifico rounded transition-all duration-1000 ease-in-out"
+        className={({ isActive }) =>
+          ` block py-2 px-4 text-gray-800 font-pacifico rounded transition-all duration-1000 ease-in-out ${
+            isActive ? setMobileOutLine(true) : setMobileOutLine(false)
+          } ${mobileOutLine ? "active" : ""}`
+        }
       >
         {name}
       </NavLink>
       <div
-        className={` ${
-          hoverEff ? "w-full" : "w-0"
+        className={` ${hoverEff ? "md:w-full" : "md:w-0"} ${
+          mobileOutLine ? "w-full" : "w-0"
         } transition-all duration-300  h-1 bg-gradient-to-r from-[#EA580C] via-[#FB923C] to-[#648F1C]  ease-in-out `}
       ></div>
     </motion.li>
