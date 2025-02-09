14e3ff9259d1e56b32c28c2c644bfa9465b6f336
blankverse123000 <blankverse123000@gmail.com>
2025-02-09 09:59:44 +0800

    feat/done gallery Page mm version

--- src/features/public/gallery/components/TopTitle.jsx
+++ src/features/public/gallery/components/TopTitle.jsx
@@ -1,30 +1,52 @@
-import React,{useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import letterImg from "../../../../assets/gallery/letterFly.svg";
-import {motion, useInView, useAnimation} from 'framer-motion'
-import { containerVariants, imageVariants, titleVariants } from "../../../../utils";
+import { motion, useInView, useAnimation } from "framer-motion";
+import {
+  containerVariants,
+  imageVariants,
+  titleVariants,
+} from "../../../../utils";
 
 const TopTitle = () => {
   const ref = useRef(null);
-    const isInView = useInView(ref, { once: true, amount: 0.1 })
-  
+  const isInView = useInView(ref, { once: true, amount: 0.1 });
+
   const controls = useAnimation();
 
- 
-   useEffect(()=> {
-      if (isInView) {
-        controls.start('visible')
-      }
-   },[controls,isInView])
+  useEffect(() => {
+    if (isInView) {
+      controls.start("visible");
+    }
+  }, [controls, isInView]);
   return (
-    <motion.div variants={containerVariants} className=" flex justify-center flex-col items-center gap-4 relative">
-      <motion.h2 ref={ref} initial='hidden' animate={controls} variants={titleVariants} className=" text-xl leading-9 font-normal text-orange-500 font-pacifico">
-        Gallery
+    <motion.div
+      variants={containerVariants}
+      className=" flex justify-center flex-col items-center gap-4 relative"
+    >
+      <motion.h2
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={titleVariants}
+        className=" text-xl leading-9 font-medium  font-pacifico"
+      >
+        ဓာတ်ပုံများ
       </motion.h2>
-      <motion.p ref={ref} initial='hidden' animate={controls} variants={titleVariants} className=" xl:text-4xl text-2xl font-poppin leading-[54px] font-semibold">
-        Our Colourful Journey
+      <motion.p
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={titleVariants}
+        className=" xl:text-4xl text-xl font-poppin leading-[54px] font-semibold"
+      >
+        အမှတ်တရရက်စွဲများ
       </motion.p>
-      <motion.img ref={ref} initial='hidden' animate={controls} variants={imageVariants}
-        className="  hidden xl:inline-block absolute bottom-2 left-0 translate-x-44 translate-y-24 rotate-3 "
+      <motion.img
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={imageVariants}
+        className="  hidden xl:inline-block absolute bottom-[-85%] left-[10%] translate-x-64 translate-y-24  rotate-3  "
         src={letterImg}
         alt=""
       />
